270ab1ff86d372e368ef75e9cb3abf2f3480550d
eureka <muisurasak93@gmail.com>
2026-06-19 18:52:30 +0700

    han: Add /about page and navigation link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,22 @@
-export const metadata = { title: 'Bottlenose Hello World' }
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html className={inter.className}>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+            </ul>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
